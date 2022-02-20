deec089db4e47df77ec7e56d067a2cfe2415600e
Chen Li <jeremyliisagoodman@gmail.com>
2022-02-20 01:37:08 -0800

    fix file matches and add unit tests

--- solaudit/matchers.py
+++ solaudit/matchers.py
@@ -12,34 +12,40 @@ pp.ParserElement.enablePackrat()
 LBRACK, RBRACK, LBRACE, RBRACE, LPAR, RPAR, EQ, COMMA, SEMI, COLON, REF = map(
     pp.Suppress, "[]{}()=,;:&"
 )
-RETURN, IF, ELSE, LET, FN, TRUE, FALSE = map(
-    pp.Suppress, ["return", "if", "else", "let", "fn", "true", "false"]
+RETURN, IF, ELSE, LET, FN, TRUE, FALSE, RETURN = map(
+    pp.Suppress, ["return", "if", "else", "let", "fn", "true", "false", "return"]
 )
-AND, OR, NOT = map(pp.Literal, ["&&", "||", "!"])
+AND, OR, NOT, SCOPE_RES = map(pp.Literal, ["&&", "||", "!", "::"])
 OPERATOR = pp.oneOf("+ - * / %")
+OPT_SEMI = pp.Optional(SEMI).suppress()
 
 comment = pp.Suppress(pp.Literal("//")) + pp.restOfLine("comment")
 string = pp.QuotedString("'") | pp.QuotedString('"')
 
 any_keyword = pp.MatchFirst(RETURN | IF | LET | FN).setName("<keyword>")
 ident = ~any_keyword + ppc.identifier("ident")
-name = pp.delimitedList(ident, delim=".", combine=True)
+name = pp.delimitedList(ident, delim=pp.Literal(".") | SCOPE_RES, combine=True)
+type_name = pp.Optional("&") + pp.Optional("[") + name + pp.Optional("]")
 
 exp = pp.Forward()
 exp_list = pp.delimitedList(exp)
 
 stat = pp.Forward()
-block = LBRACE + pp.Group(stat + SEMI)[1, ...] + RBRACE
+block = pp.Group(stat + SEMI)[...] + pp.Group(stat + OPT_SEMI)[...]
 
-param = ident + COLON + exp
+param = ident + COLON + type_name
 param_list = pp.delimitedList(param)
 
-func_head = FN + name + pp.Group(LPAR + param_list + RPAR) + pp.Suppress("->") + name
-function_def = func_head + block
-function_call = name + pp.Group(LPAR + exp_list + RPAR)
+func_head = (
+    FN + name + pp.Group(LPAR + param_list + RPAR) + pp.Suppress("->") + type_name
+)
+ok_stat = pp.Literal("Ok(())")
+function_body = LBRACE + block + pp.Optional(ok_stat + OPT_SEMI) + RBRACE
+function_def = func_head + function_body
+function_call = name + pp.Group(LPAR + pp.Optional(exp_list) + RPAR)
 
 var = pp.Forward()
-var_atom = function_call | name | LPAR + exp + RPAR | LBRACK + name + RBRACK
+var_atom = function_call | name | LPAR + exp + RPAR
 index_ref = pp.Group(LBRACK + exp + RBRACK)
 var <<= pp.delimitedList(pp.Group(var_atom + index_ref) | var_atom, delim=".")
 
@@ -55,17 +61,35 @@ exp <<= pp.infixNotation(
     ],
 )
 
-assignment_stat = pp.Optional(LET) + var + pp.Optional(OPERATOR) + EQ + exp
+assignment_stat = (
+    pp.Optional(LET)
+    + var
+    + pp.Optional(pp.Group(COLON + type_name))
+    + pp.Optional(OPERATOR)
+    + EQ
+    + exp
+)
 if_stat = (
     IF
-    + LPAR
     + exp
-    + RPAR
+    + LBRACE
     + block
-    + pp.Optional(pp.Group(ELSE + IF + LPAR + exp + RPAR)[...])
-    + pp.Optional(pp.Group(ELSE + block))
+    + RBRACE
+    + pp.Optional(pp.Group(ELSE + IF + exp + LBRACE + block + RBRACE)[...])
+    + pp.Optional(pp.Group(ELSE + LBRACE + block + RBRACE))
+)
+return_stat = RETURN + exp + SEMI
+
+stat <<= pp.Group(
+    assignment_stat | function_call | if_stat | function_def | comment | return_stat
 )
 
-stat <<= pp.Group(assignment_stat | function_call | if_stat | function_def)
+solana_file = pp.Group(stat + SEMI)[...] + pp.Group(stat + OPT_SEMI)[...]
 
-solana_file = stat[...]
+
+def printInfo(s: str, loc: int, tokens: pp.ParseResults):
+    print("===========")
+    return "hahaha"
+
+
+solana_file.setParseAction(printInfo)
--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -1,4 +1,3 @@
-from typing import Dict
 from solaudit.matchers import *
 
 
@@ -8,11 +7,13 @@ def test_comment():
     assert comment.parseString("//" + content).asDict()["comment"] == content
 
 
-def test_identifie():
-    """Test parsing identifier."""
-    assert ident.parseString("abc").asDict()["ident"] == "abc"
-    assert ident.parseString("abc123").asDict()["ident"] == "abc123"
-    assert ident.parseString("abc_123").asDict()["ident"] == "abc_123"
+def test_name():
+    """Test parsing names."""
+    assert name.parseString("abc")[0] == "abc"
+    assert name.parseString("abc123")[0] == "abc123"
+    assert name.parseString("abc_123")[0] == "abc_123"
+    assert name.parseString("abc.def")[0] == "abc.def"
+    assert name.parseString("abc::def")[0] == "abc::def"
 
 
 def test_expression():
@@ -26,20 +27,48 @@ def test_expression():
 def test_statement():
     """Test parsing statement."""
     assign_statement_1 = "let a = b + c;"
-    assign_statement_2 = "a += b + c;"
+    assign_statement_2 = "let a: A = b + c;"
+    assign_statement_3 = "a += b + c;"
     if_statement = """
-        if (a + b > 0) {
-            c = 1;
+        if a > b {
+            c = a;
         }
     """
     function_call_statement = "abc(1, 2, 3)"
     function_def_statement = """
-        fn abc(program_id: &Pubkey, accounts: &[AccountInfo], amount: u32) -> ProgramResult {
-            let a = 1;
+        fn test(program_id: &Pubkey, accounts: &[AccountInfo], amount: u32) -> ProgramResult {            
+            if a + b > 0 {
+                let c = 1;
+            }
+            let d = 2;
+
+            Ok(())
         }
     """
+    return_statement = "return a;"
+    stat.ignore(comment)
     assert len(stat.parseString(assign_statement_1).asList()) == 1
     assert len(stat.parseString(assign_statement_2).asList()) == 1
+    assert len(stat.parseString(assign_statement_3).asList()) == 1
     assert len(stat.parseString(if_statement).asList()) == 1
     assert len(stat.parseString(function_call_statement).asList()) == 1
     assert len(stat.parseString(function_def_statement).asList()) == 1
+    assert len(stat.parseString(return_statement).asList()) == 1
+
+
+def test_file():
+    """Test parsing solana file."""
+    file_content = """
+        // this is a comemnt.
+        let a: A = 100;
+        fn test(program_id: &Pubkey, accounts: &[AccountInfo], amount: u32) -> ProgramResult {            
+            if a + b > 0 {
+                let c = 1;
+            }
+            let d = 2;
+
+            Ok(())
+        }
+    """
+    solana_file.ignore(comment)
+    assert len(solana_file.parseString(file_content)) == 2
