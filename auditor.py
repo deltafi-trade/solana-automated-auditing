e8f5a82ebaca2aead90e7bd098695deaac25443e
Chen Li <jeremyliisagoodman@gmail.com>
2022-02-20 22:39:56 -0800

    clean up

--- auditor.py
+++ auditor.py
@@ -8,28 +8,6 @@ from solaudit.parsers import getProgramParser, comment
 from solaudit.models import Program
 
 
-LBRACK, RBRACK, LBRACE, RBRACE, LPAR, RPAR, EQ, COMMA, SEMI, COLON, QUESTION = map(
-    Suppress, "[]{}()=,;:?"
-)
-
-
-def missingSignerCheck(content: str) -> None:
-    identifier = Word(alphanums + "_")
-
-    # Check if fetching account.
-    next_account_info = Keyword("next_account_info")
-    account_right_expr = ... + next_account_info * 1 + ... + SEMI
-    account_assignment = Suppress("let") + identifier + EQ + account_right_expr
-    is_fetcing_account = len(account_assignment.searchString(content)) > 0
-
-    # Check if checking signer.
-    is_signer = Keyword("is_signer")
-    is_checking_signer = len(is_signer.searchString(content)) > 0
-
-    if is_fetcing_account and not is_checking_signer:
-        print("Warning: Missing singer check!")
-
-
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-f", dest="filename", default="")
